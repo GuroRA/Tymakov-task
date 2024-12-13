ea397fe614abd4b84c38713522bafa1440d544f9
firman <rus.guro123@gmail.com>
2024-12-13 22:43:34 +0300

    Добавьте файлы проекта.

---
+++ Tymakov-task/Dweller.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tymakov_task
+{
+    struct Problem
+    {
+        public Guid IdOfProblem { get; set; }
+        public string Description { get; set; }
+    }
+
+    public struct Temperament
+    {
+        private byte level;
+        public byte Level
+        {
+            get
+            {
+                return level;
+            }
+            set
+            {
+                if (value > 10)
+                {
+                    level = 10;
+                    return;
+                }
+                else if (value < 0)
+                {
+                    level = 0;
+                    return;
+                }
+                level = value;
+            }
+        }
+        private byte wisdom;
+        public byte Wisdom
+        {
+            get { return wisdom; }
+            set
+            {
+                if (value > 1)
+                {
+                    wisdom = 1;
+                    return;
+                }
+                else if (value < 0)
+                {
+                    wisdom = 0;
+                    return;
+                }
+                wisdom = value;
+            }
+        }
+    }
+    class Dweller
+    {
+        public string Name { get; set; }
+        public Guid PasportID { get; set; }
+        public Problem Problem;
+        public Temperament Temperament;
+        public static Stack<Dweller> Objects = new Stack<Dweller>();
+
+        public Dweller(string name, Guid pasportId, Guid idOfProblem, string problemDiscription, byte tempLevel, byte wisdom)
+        {
+            Name = name;
+            PasportID = pasportId;
+            Problem.IdOfProblem = idOfProblem;
+            Problem.Description = problemDiscription;
+            Temperament.Wisdom = wisdom;
+            Temperament.Level = tempLevel;
+            Objects.Push(this);
+        }
+        
+    }
+}
---
+++ Tymakov-task/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tymakov_task
+{
+    internal class Program
+    {
+        static void Zina(Stack<Dweller> dwellers, Queue<Dweller> window1, Queue<Dweller> window2, Queue<Dweller> window3)
+        {
+            while (dwellers.Count > 0)
+            {
+                Dweller dweller = dwellers.Pop();
+
+                if (dweller.Temperament.Wisdom == 0)
+                { 
+                    int rnd = new Random().Next(1, 3);
+                    switch (rnd)
+                    {
+                        case 1:
+                            window1.Enqueue(dweller);
+                            break;
+                        case 2:
+                            window2.Enqueue(dweller);
+                            break;
+                        case 3:
+                            window3.Enqueue(dweller);
+                            break;
+                    }
+                }
+                else if (dweller.Problem.Description.ToLower().Contains("отоплен"))
+                {
+                    window1.Enqueue(dweller);
+                }
+                else if (dweller.Problem.Description.ToLower().Contains("оплат"))
+                {
+                    window2.Enqueue(dweller);
+                }
+                else 
+                {
+                    window3.Enqueue(dweller);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Queue<Dweller> window1 = new Queue<Dweller>();
+            Queue<Dweller> window2 = new Queue<Dweller>();
+            Queue<Dweller> window3 = new Queue<Dweller>();
+
+            Dweller Pasha = new Dweller("Паша", Guid.NewGuid(), Guid.NewGuid(), "Проблемы с оплатой", 4, 1);
+            Dweller Mark = new Dweller("Марк", Guid.NewGuid(), Guid.NewGuid(), "Нет отопления", 3, 0);
+            Dweller Sophia = new Dweller("Софья", Guid.NewGuid(), Guid.NewGuid(), "Нет холодной воды", 4, 24);
+            Dweller Leroy = new Dweller("Лерой", Guid.NewGuid(), Guid.NewGuid(), "Нет отопления", 36, 1);
+            Dweller Sergey = new Dweller("Сергей", Guid.NewGuid(), Guid.NewGuid(), "Забился сортир", 8, 1);
+            Dweller Tom = new Dweller("Том", Guid.NewGuid(), Guid.NewGuid(), "Проблемы с оплатой", 7, 0);
+
+            Zina(Dweller.Objects, window1, window2, window3);
+
+            while (window1.Count > 0)
+            {
+                Console.WriteLine(window1.Dequeue().Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
